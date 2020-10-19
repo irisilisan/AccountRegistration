7d884330f457e8306ee83d12d5cad7b4b257b4d3
irisilisan <71169421+irisilisan@users.noreply.github.com>
2020-10-19 22:53:29 +0800

    Update frmConfirmation.Designer.cs

--- HandsOnActivity01/frmConfirmation.Designer.cs
+++ HandsOnActivity01/frmConfirmation.Designer.cs
@@ -2,15 +2,10 @@
 {
     partial class frmConfirmation
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+      
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +17,7 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+      
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
@@ -44,137 +36,105 @@
             this.lblContactNo = new System.Windows.Forms.Label();
             this.btnSubmit = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
+         
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(26, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "StudentNo.:";
-            // 
-            // label2
-            // 
+           
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(26, 79);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Name:";
-            // 
-            // label3
-            // 
+           
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(25, 119);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Program:";
-            // 
-            // label4
-            // 
+            
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(26, 158);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(29, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "Age:";
-            // 
-            // label5
-            // 
+           
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(26, 200);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(48, 13);
             this.label5.TabIndex = 4;
             this.label5.Text = "Birthday:";
-            // 
-            // label6
-            // 
+           
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(29, 240);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(45, 13);
             this.label6.TabIndex = 5;
             this.label6.Text = "Gender:";
-            // 
-            // label7
-            // 
+           
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(26, 273);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(64, 13);
             this.label7.TabIndex = 6;
             this.label7.Text = "ContactNo.:";
-            // 
-            // lblStudentNo
-            // 
+           
             this.lblStudentNo.AutoSize = true;
             this.lblStudentNo.Location = new System.Drawing.Point(148, 41);
             this.lblStudentNo.Name = "lblStudentNo";
             this.lblStudentNo.Size = new System.Drawing.Size(0, 13);
             this.lblStudentNo.TabIndex = 7;
-            // 
-            // lblName
-            // 
+           
             this.lblName.AutoSize = true;
             this.lblName.Location = new System.Drawing.Point(148, 79);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(0, 13);
             this.lblName.TabIndex = 8;
-            // 
-            // lblProgram
-            // 
+          
             this.lblProgram.AutoSize = true;
             this.lblProgram.Location = new System.Drawing.Point(148, 119);
             this.lblProgram.Name = "lblProgram";
             this.lblProgram.Size = new System.Drawing.Size(0, 13);
             this.lblProgram.TabIndex = 9;
-            // 
-            // lblAge
-            // 
+           
             this.lblAge.AutoSize = true;
             this.lblAge.Location = new System.Drawing.Point(148, 158);
             this.lblAge.Name = "lblAge";
             this.lblAge.Size = new System.Drawing.Size(0, 13);
             this.lblAge.TabIndex = 10;
-            // 
-            // lblBirthday
-            // 
+           
             this.lblBirthday.AutoSize = true;
             this.lblBirthday.Location = new System.Drawing.Point(147, 200);
             this.lblBirthday.Name = "lblBirthday";
             this.lblBirthday.Size = new System.Drawing.Size(0, 13);
             this.lblBirthday.TabIndex = 12;
-            // 
-            // lblGender
-            // 
+          
             this.lblGender.AutoSize = true;
             this.lblGender.Location = new System.Drawing.Point(148, 240);
             this.lblGender.Name = "lblGender";
             this.lblGender.Size = new System.Drawing.Size(0, 13);
             this.lblGender.TabIndex = 13;
-            // 
-            // lblContactNo
-            // 
+          
             this.lblContactNo.AutoSize = true;
             this.lblContactNo.Location = new System.Drawing.Point(147, 273);
             this.lblContactNo.Name = "lblContactNo";
             this.lblContactNo.Size = new System.Drawing.Size(0, 13);
             this.lblContactNo.TabIndex = 14;
-            // 
-            // btnSubmit
-            // 
+           
             this.btnSubmit.Location = new System.Drawing.Point(196, 348);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(75, 23);
             this.btnSubmit.TabIndex = 15;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = true;
-            // 
-            // frmConfirmation
-            // 
+           
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(313, 399);
